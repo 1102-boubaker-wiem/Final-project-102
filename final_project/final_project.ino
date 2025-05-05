f99540b09ffe8be74123afd84f8bd09c6d0f1b9d
1102-boubaker-wiem <boubakerwiem@gmail.com>
2025-05-05 16:01:00 -0700

    Full state machine, ADC for water + all LED controls + UARsages

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -1,10 +1,17 @@
-//Wiem Boubaker
-//Josue Santa Cruz
-//CPE 301
+// Wiem Boubaker
+// Josue Santa Cruz Ramirez
+// CPE 301 - Final Project
 
-// ---------- UART Definitions ----------
 #define RDA 0x80
 #define TBE 0x20
+
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include <DHT.h>  // Requires Arduino DHT sensor library
+
+#define DHTPIN 22     // DHT11 connected to digital pin 22
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 volatile unsigned char* myUCSR0A = (unsigned char*) 0xC0;
 volatile unsigned char* myUCSR0B = (unsigned char*) 0xC1;
@@ -12,43 +19,6 @@ volatile unsigned char* myUCSR0C = (unsigned char*) 0xC2;
 volatile unsigned int*  myUBRR0  = (unsigned int*)  0xC4;
 volatile unsigned char* myUDR0   = (unsigned char*) 0xC6;
 
-// ---------- UART Functions ----------
-void U0Init(int U0baud)
-{
-  unsigned long FCPU = 16000000;
-  unsigned int tbaud = (FCPU / 16 / U0baud - 1);
-  *myUCSR0A = 0x20; // Clear status flags
-  *myUCSR0B = 0x18; // Enable TX and RX
-  *myUCSR0C = 0x06; // 8N1
-  *myUBRR0  = tbaud;
-}
-
-unsigned char kbhit()
-{
-  return (*myUCSR0A & RDA) ? 1 : 0;
-}
-
-unsigned char getChar()
-{
-  while (!kbhit()) {}
-  return *myUDR0;
-}
-
-void putChar(unsigned char U0pdata)
-{
-  while (!(*myUCSR0A & TBE)) {}
-  *myUDR0 = U0pdata;
-}
-
-void putString(const char* str)
-{
-  while (*str)
-  {
-    putChar(*str++);
-  }
-}
-
-// ---------- GPIO Setup ----------
 volatile unsigned char* ddr_k  = (unsigned char*) 0x107;
 volatile unsigned char* pin_k  = (unsigned char*) 0x106;
 volatile unsigned char* port_k = (unsigned char*) 0x108;
@@ -56,62 +26,146 @@ volatile unsigned char* port_k = (unsigned char*) 0x108;
 volatile unsigned char* ddr_l  = (unsigned char*) 0x10A;
 volatile unsigned char* port_l = (unsigned char*) 0x10B;
 
-// ---------- Global Variables ----------
-volatile bool isOn = false;
+volatile unsigned char* my_ADMUX   = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB  = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA  = (unsigned char*) 0x7A;
+volatile unsigned int*  my_ADC_DATA = (unsigned int*) 0x78;
 
-// ---------- ISR for Button Press ----------
-ISR(PCINT2_vect)
-{
-  bool buttonState = (*pin_k & (1 << 2)) == 0; // Active LOW
+enum SystemState { DISABLED, IDLE, RUNNING, ERROR };
+volatile SystemState currentState = DISABLED;
 
-  if (buttonState)
-  {
-    isOn = !isOn;
-
-    if (isOn)
-      putString("System ON\r\n");
-    else
-      putString("System OFF\r\n");
-  }
+ISR(PCINT2_vect) {
+    bool buttonState = (*pin_k & (1 << 2)) == 0;
+    if (buttonState) {
+        if (currentState == DISABLED) {
+            currentState = IDLE;
+            putString("System STARTED -> IDLE\\r\\n");
+        } else {
+            currentState = DISABLED;
+            putString("System STOPPED -> DISABLED\\r\\n");
+        }
+    }
 }
 
-// ---------- Setup ----------
-void setup()
-{
-  cli(); // Disable interrupts
+void setup() {
+    cli();
+    U0init(9600);
+    adc_init();
+    dht.begin();
 
-  // UART
-  U0Init(9600);
+    *ddr_k &= ~(1 << 2);  // PK2 as input
+    *port_k |= (1 << 2);  // pull-up
 
-  // Set PK2 as input and enable pull-up
-  *ddr_k &= ~(1 << 2);
-  *port_k |= (1 << 2);
+    *ddr_l |= 0x0F;  // PL0-PL3 output
+    *port_l &= ~0x0F; // All LEDs OFF
 
-  // Set digital 44 (PL5) and 46 (PL3) as outputs
-  *ddr_l |= (1 << 5) | (1 << 3);
-
-  // Initially ON
-  *port_l |= (1 << 5); // LED1 ON
-  *port_l &= ~(1 << 3); // LED2 OFF
-
-  // Enable Pin Change Interrupt on PK2
-  PCICR |= (1 << PCIE2);     // Enable PCINT[23:16]
-  PCMSK2 |= (1 << PCINT18);  // Enable PCINT18 (PK2)
-
-  sei(); // Enable global interrupts
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT18);
+    sei();
 }
 
-// ---------- Main Loop ----------
-void loop()
-{
-  if (isOn)
-  {
-    *port_l &= ~(1 << 5); // LED1 OFF
-    *port_l |= (1 << 3);  // LED2 ON
-  }
-  else
-  {
-    *port_l |= (1 << 5);  // LED1 ON
-    *port_l &= ~(1 << 3); // LED2 OFF
-  }
+void loop() {
+    switch (currentState) {
+        case DISABLED:
+            setLEDState(DISABLED);
+            break;
+
+        case IDLE:
+            setLEDState(IDLE);
+            putString("State: IDLE\\r\\n");
+
+            if (adc_read(0) < 100) {
+                currentState = ERROR;
+                putString("-> ERROR: Water level LOW\\r\\n");
+            }
+
+            float temp = dht.readTemperature();
+            if (!isnan(temp)) {
+                if (temp > 25.0) {
+                    currentState = RUNNING;
+                    putString("Temp > 25C -> RUNNING\\r\\n");
+                }
+            }
+
+            delay_ms(1000);
+            break;
+
+        case RUNNING:
+            setLEDState(RUNNING);
+            putString("State: RUNNING\\r\\n");
+
+            if (adc_read(0) < 100) {
+                currentState = ERROR;
+                putString("-> ERROR: Water level LOW\\r\\n");
+            }
+
+            float currTemp = dht.readTemperature();
+            if (!isnan(currTemp) && currTemp <= 25.0) {
+                currentState = IDLE;
+                putString("Temp <= 25C -> IDLE\\r\\n");
+            }
+
+            delay_ms(1000);
+            break;
+
+        case ERROR:
+            setLEDState(ERROR);
+            putString("ERROR: Low water level\\r\\n");
+
+            delay_ms(1000);
+            break;
+    }
+}
+
+void setLEDState(SystemState state) {
+    *port_l &= ~0x0F;
+    switch (state) {
+        case DISABLED: *port_l |= (1 << 0); break;
+        case IDLE:     *port_l |= (1 << 1); break;
+        case RUNNING:  *port_l |= (1 << 3); break;
+        case ERROR:    *port_l |= (1 << 2); break;
+    }
+}
+
+void putString(const char* str) {
+    while (*str) putChar(*str++);
+}
+
+void putChar(unsigned char c) {
+    while (!(*myUCSR0A & TBE));
+    *myUDR0 = c;
+}
+
+void U0init(int baud) {
+    unsigned long FCPU = 16000000;
+    unsigned int tbaud = (FCPU / 16 / baud - 1);
+    *myUCSR0A = 0x20;
+    *myUCSR0B = 0x18;
+    *myUCSR0C = 0x06;
+    *myUBRR0  = tbaud;
+}
+
+void adc_init() {
+    *my_ADCSRA |= (1 << 7);
+    *my_ADCSRA &= ~(1 << 5);
+    *my_ADCSRA &= ~(1 << 3);
+    *my_ADCSRA |= (1 << 2) | (1 << 1) | (1 << 0);
+    *my_ADCSRB &= ~(1 << 3);
+    *my_ADCSRB &= ~((1 << 2) | (1 << 1) | (1 << 0));
+    *my_ADMUX |= (1 << 6);
+    *my_ADMUX &= ~(1 << 5);
+    *my_ADMUX &= 0xE0;
+}
+
+unsigned int adc_read(unsigned char ch) {
+    *my_ADMUX = (*my_ADMUX & 0xE0) | (ch & 0x1F);
+    *my_ADCSRA |= (1 << 6);
+    while ((*my_ADCSRA & (1 << 6)));
+    return *my_ADC_DATA;
+}
+
+void delay_ms(unsigned int ms) {
+    for (unsigned int i = 0; i < ms; i++)
+        for (unsigned int j = 0; j < 1600; j++)
+            asm volatile("nop");
 }
